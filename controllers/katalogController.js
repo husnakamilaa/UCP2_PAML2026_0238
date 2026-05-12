d61eed7f33393b1afd5cb031b414bcd78fdea494
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-12 10:02:37 +0700

    feat: buat katalog controller untuk api method cruds

---
+++ controllers/katalogController.js
@@ -0,0 +1,97 @@
+const db = require("../config/database")
+
+// create
+exports.createKatalog = (req, res) => {
+  const { nama, harga, tahun_produksi, transmisi, capacity, maxspeed, image } = req.body;
+
+  const sql = `
+    INSERT INTO katalog (nama, harga, tahun_produksi, transmisi, capacity, maxspeed, image)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
+  `;
+
+  db.query(sql, [nama, harga, tahun_produksi, transmisi, capacity, maxspeed, image], (err) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Gagal menambah katalog",
+        error: err.message
+      });
+    }
+
+    res.status(201).json({
+      message: "Katalog berhasil ditambahkan"
+    });
+  });
+};
+
+// read all
+exports.getAllKatalog = (req, res) => {
+  const sql = "SELECT * FROM katalog";
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    res.json(results);
+  });
+};
+
+// search by nama
+exports.searchKatalog = (req, res) => {
+  const { nama } = req.query;
+
+  const sql = "SELECT * FROM katalog WHERE nama LIKE ?";
+  db.query(sql, [`%${nama}%`], (err, results) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    res.json(results);
+  });
+};
+
+// get by id
+exports.getKatalogById = (req, res) => {
+  const { id } = req.params;
+
+  const sql = "SELECT * FROM katalog WHERE id = ?";
+  db.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json(err);
+
+    if (results.length === 0) {
+      return res.status(404).json({
+        message: "Katalog tidak ditemukan"
+      });
+    }
+
+    res.json(results[0]);
+  });
+};
+
+// update
+exports.updateKatalog = (req, res) => {
+  const { id } = req.params;
+  const { nama, harga, tahun_produksi, transmisi, capacity, maxspeed, image, } = req.body;
+
+  const sql = `
+    UPDATE katalog
+    SET nama = ?, harga = ?, tahun_produksi = ?, transmisi = ?, capacity = ?, maxspeed = ?, image = ?
+    WHERE id = ?
+  `;
+
+  db.query(sql, [nama, harga, tahun_produksi, transmisi, capacity, maxspeed, image, id], (err) => {
+    if (err) return res.status(500).json(err);
+
+    res.json({ message: "Data katalog berhasil diupdate" });
+  });
+};
+
+// delete
+exports.deleteKatalog = (req, res) => {
+  const { id } = req.params;
+
+  const sql = "DELETE FROM katalog WHERE id = ?";
+  db.query(sql, [id], (err) => {
+    if (err) return res.status(500).json(err);
+
+    res.json({ message: "Data katalog berhasil dihapus" });
+  });
+};
