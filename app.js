57d3a4fc4315e76d5c5beb8634c89c855af2a2a6
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-12 10:07:49 +0700

    feat: bash path buat auth, katalog, dan katalog customer

---
+++ app.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const katalogRoutes = require("./routes/katalogRoutes");
+const customerRoutes = require("./routes/customerRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API DriveEase running" });
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/katalog", katalogRoutes);
+app.use("/api/katalog/customer", customerRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
