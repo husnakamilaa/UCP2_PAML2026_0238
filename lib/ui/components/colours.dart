a294cbaec98f409d737e8dfe7415e49d0371fbc7
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-05-15 00:47:56 +0700

    feat: component buat colour palette

---
+++ lib/ui/components/colours.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color navy = Color(0xFF001F3F);
+  static const Color white = Color(0xFFF8FAFC);
+  static const Color grey = Color(0xFF64748B);
+  static const Color navygrey = Color(0xFF1E293B);  
+  static const Color lightblue = Color(0xFFEBF1FF);
+}
